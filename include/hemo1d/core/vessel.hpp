6c827aed3e71e11dcfc3374209b3760fd1f200c0
Min <mjeon@uc.cl>
2026-08-14 23:43:25 -0400

    vessel structure

---
+++ include/hemo1d/core/vessel.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <optional>
+#include <string>
+
+#include "hemo1d/core/types.hpp"
+
+namespace hemo1d {
+    // Which extreme of the vessel a node is
+    enum class VesselEnd {Proximal, distal};
+
+    // Discretization parameters:
+
+    struct VesselParameters { 
+        Real length = 0.0;
+        Real A0 = 0.0;
+        Real beta = 0.0;
+        Real alpha = 4.0/3.0;
+        Real frictionKr = 0.0;
+
+        Index nElements = 1;
+    };
+
+    class Vessel {
+        private:
+            Id id_;
+            std::string name_;
+            VesselParameters params_;
+        
+        public:
+            Vessel(Id id, std::string name, VesselParameters params);
+ 
+            Id id() const noexcept { return id_,}
+            const std::string& name() const noexcept { return name_;}
+            const VesselParameters& parameters() const noexcept {return params_,}
+    };
+
+} //namespace hemo1d
